8830410336571728513904bdad4dde8537e5d34b
Dicky Perdian <dickydraknes@gmail.com>
2026-03-09 14:29:27 +0700

    Test password

--- tests/e2e/api/login.spec.js
+++ tests/e2e/api/login.spec.js
@@ -3,10 +3,13 @@ import { test, expect } from '@playwright/test';
 test('login testes', async ({ request }) => {
   const response = await request.post('http://127.0.0.1:3100/auth/login', {
     data: {
-      email: 'dickydraknes@gmail.com',
-      password: 'asder122s'
+      email: process.env.TEST_USER_EMAIL,
+      password: process.env.TEST_USER_PASSWORD
     }
   });
+
+  
+
 
   expect(response.status()).toBe(200);
   const body = await response.json();
